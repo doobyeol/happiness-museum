5f9f43a2bf32dba8ebe9f3d0e0c77f0e76464e2f
Doobyeol <doobyeol@gmail.com>
2022-09-04 17:39:30 +0900

    ADD: diary detail

---
+++ front/src/views/diary/DiaryDetailView.vue
@@ -0,0 +1,38 @@
+<template>
+	<v-container class="mt-6">
+		상세보기 페이지
+		<h1 v-if="isWrite">isWrite</h1>
+		<h1 v-if="isRead">isRead</h1>
+		<h1 v-if="isModify">isModify</h1>
+	</v-container>
+</template>
+
+<script>
+export default {
+	props: {
+		isRead: {
+			type: Boolean,
+			default: false,
+			require: false,
+		},
+		isWrite: {
+			type: Boolean,
+			default: false,
+			require: false,
+		},
+		isModify: {
+			type: Boolean,
+			default: false,
+			require: false,
+		},
+	},
+	// data() {
+	// 	return {
+	// 		key: value,
+	// 	};
+	// },
+	// created() {},
+};
+</script>
+
+<style></style>
